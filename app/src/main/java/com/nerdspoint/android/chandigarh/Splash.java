d9f7af53e0be33e41b41b982f4286ecbd5f03465
baljeet808 <baljeet808singh@gmail.com>
2017-03-02 15:16:04 +0530

    Rahul shadow

--- app/src/main/java/com/nerdspoint/android/chandigarh/Splash.java
+++ app/src/main/java/com/nerdspoint/android/chandigarh/Splash.java
@@ -1,19 +1,31 @@
 package com.nerdspoint.android.chandigarh;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
 
 
     //create objects here
 
+    TextView textView;
+    public Typeface typeface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        textView = (TextView) findViewById(R.id.textView6);
+
+
+         typeface=  Typeface.createFromAsset(getAssets(),"waltograph42.ttf");
+        textView.setTypeface(typeface);
+
 
         // initialize objects here  or attach there xml ids
 
