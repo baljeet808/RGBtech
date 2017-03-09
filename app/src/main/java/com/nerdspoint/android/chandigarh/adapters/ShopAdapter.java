10a284f9b00c6fe1e130f03cc2d243e6727442e7
baljeet808 <baljeet808singh@gmail.com>
2017-03-09 15:38:41 +0530

    Merge https://github.com/baljeet808/RGBtech
    # Conflicts:
    #	app/src/main/java/com/nerdspoint/android/chandigarh/activities/LoginActivity.java
    #	app/src/main/java/com/nerdspoint/android/chandigarh/activities/SignupForm.java
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_signup_form.xml

---
+++ app/src/main/java/com/nerdspoint/android/chandigarh/adapters/ShopAdapter.java
@@ -0,0 +1,74 @@
+package com.nerdspoint.android.chandigarh.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.nerdspoint.android.chandigarh.R;
+import com.nerdspoint.android.chandigarh.sharedPrefs.ShopDetails;
+
+import java.util.List;
+
+/**
+ * Created by android on 09-03-2017.
+ */
+
+public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.MyViewHolder>{
+
+
+    Context context;
+    List<ShopDetails> list;
+
+    public ShopAdapter(Context context, List<ShopDetails> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view= LayoutInflater.from(context).inflate(R.layout.cardviewforsearch,parent,false);
+        return new ShopAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ShopDetails details = list.get(position);
+        String shopName = details.shopName;
+        String product = details.productName;
+        String price = details.price;
+        String contactNumber = details.contactNumber;
+        String address = details.address;
+
+        holder.shopName.setText(shopName);
+        holder.contactNumber.setText(contactNumber);
+        holder.address.setText(address);
+        holder.price.setText(price);
+        holder.product.setText(product);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView shopName,product,price,address,contactNumber;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            shopName=(TextView) itemView.findViewById(R.id.shopNameHolder);
+            price=(TextView) itemView.findViewById(R.id.priceHolder);
+            product=(TextView) itemView.findViewById(R.id.productHolder);
+            address=(TextView) itemView.findViewById(R.id.AddressHolder);
+            contactNumber=(TextView) itemView.findViewById(R.id.ContactNumberHolder);
+        }
+    }
+}
