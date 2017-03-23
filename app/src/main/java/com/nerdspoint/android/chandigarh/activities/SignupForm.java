134f1a530895f403b01943338629f259e92b1a13
baljeet808 <baljeet808singh@gmail.com>
2017-03-23 10:49:53 +0530

    gaurav

--- app/src/main/java/com/nerdspoint/android/chandigarh/activities/SignupForm.java
+++ app/src/main/java/com/nerdspoint/android/chandigarh/activities/SignupForm.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -26,6 +27,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 import com.nerdspoint.android.chandigarh.R;
 import com.nerdspoint.android.chandigarh.fragments.profileUpdation;
 import com.nerdspoint.android.chandigarh.sharedPrefs.ActiveUserDetail;
@@ -66,7 +71,22 @@ public class SignupForm extends AppCompatActivity {
         Password=(EditText) findViewById(R.id.password);
         ConfirmPassword=(EditText) findViewById(R.id.confirmpassword);
 
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setText("Verify your phone Number");
 
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Au.thentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
 
     }
 
